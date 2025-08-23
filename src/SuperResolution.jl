36b6d3dce38a34499e2252c2030051093cfee80c
cirobr <ciro.rosa@alumni.usp.br>
2025-08-23 17:58:58 -0300

    add leakyrelu

--- src/SuperResolution.jl
+++ src/SuperResolution.jl
@@ -4,7 +4,7 @@ module SuperResolution
 export edsrmodel, EDSRBaseline, EDSRExpanded
 
 import Flux
-import Flux: Chain, SkipConnection, relu, sigmoid, @layer
+import Flux: Chain, SkipConnection, relu, sigmoid, leakyrelu, @layer
 import TinyMachines: ConvK3
 
 include("blocks.jl")
